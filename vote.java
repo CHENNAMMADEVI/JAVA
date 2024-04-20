e3d5d6e34c105f46cf9bb36dd41e40a19a0b06b9
CHENNAMMADEVI <167308428+CHENNAMMADEVI@users.noreply.github.com>
2024-04-20 20:21:56 +0530

    Add files via upload

---
+++ vote.java
@@ -0,0 +1,21 @@
+class vote 
+{
+
+public static void main(String args[])
+{
+String name= "Devi";
+System.out.println("your name is "+ name);
+
+String place= "chennai";
+System.out.println("your place is "+ place);
+
+int age =17;
+
+if (age>= 18)
+{
+System.out.println("you are eligible to vote");
+}
+else
+System.out.println("you are not eligible to vote");
+}
+}
