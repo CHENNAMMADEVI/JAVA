71b3f84371552d7a8825061f14c29a9bd4508369
CHENNAMMADEVI <167308428+CHENNAMMADEVI@users.noreply.github.com>
2024-04-24 17:33:18 +0530

    Add files via upload

---
+++ votee.java
@@ -0,0 +1,16 @@
+import java .util.*;
+public class votee{
+public static void main(String[]args)
+{
+Scanner name= new Scanner(System.in);
+System.out.println("enter the value:");
+int n= name.nextInt();
+name.nextLine();
+for(int i=0;i<n;i++)
+{
+System.out.println(i);
+System.out.println("enter the name:");
+String n1=name.nextLine();
+}
+}
+}
