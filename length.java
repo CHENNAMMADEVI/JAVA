c358037a9b0ba5c1abb05a6b01aee17836b0bf45
CHENNAMMADEVI <167308428+CHENNAMMADEVI@users.noreply.github.com>
2024-04-26 17:55:46 +0530

    Add files via upload

---
+++ length.java
@@ -0,0 +1,12 @@
+public class length
+{
+ public static void main(String[]args)
+ {
+  String a="chennammadevi";
+  for(int i=0;i<a.length();i++)
+{  
+if(a.charAt(i)!='a' && a.charAt(i)!='d' && a.charAt(i)!='c')
+  
+   System.out.println(a.charAt(i));
+ }
+}}
