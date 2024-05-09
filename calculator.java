b9fd3bbc78678891cfa5a109745b9114caca362c
CHENNAMMADEVI <167308428+CHENNAMMADEVI@users.noreply.github.com>
2024-05-09 17:36:40 +0530

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,47 @@
+package pr1;
+import java.util.Scanner;
+
+public class calculator1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter second number:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Enter an operator (+, -, *, /):");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero!");
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator!");
+        }
+
+        scanner.close();
+    }
+}
+
+
